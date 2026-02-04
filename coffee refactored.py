c4428753ae59d2c37813dcc7a6845ec5dbe093b3
Edis B <eb614@kent.ac.uk>
2026-02-04 16:23:41 +0000

    Done

--- coffee refactored.py
+++ coffee refactored.py
@@ -21,13 +21,20 @@ def customer_input():
         coins = int(input("Insert Coins: "))
 
         if coins in [50, 20, 10, 5]:
-            total_inserted += coins
-            print(f"Total inserted: {total_inserted}p")
-            print("-------------------------------------")
+            update_total(coins)
             if total_inserted >= 75:
                 change_calculator()
         else:
             print("Invalid coin. Please insert from the above.")
+
+
+def update_total(coins):
+
+    global total_inserted
+
+    total_inserted += coins
+    print(f"Total inserted: {total_inserted}p")
+    print("-------------------------------------")
 
 
     
